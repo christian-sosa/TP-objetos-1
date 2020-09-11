96a78eaf5e13ea3a97bb91935fb837f174c3dcaf
rabistrof <alancano123@hotmail.com>
2020-09-11 11:32:22 -0300

    Articulo, cliente, item, ubic

---
+++ src/modelo/ItemCarrito.java
@@ -0,0 +1,27 @@
+package modelo;
+
+public class ItemCarrito {
+    private Articulo articulo;
+    private int cantidad;
+
+    public ItemCarrito(Articulo articulo, int cantidad) {
+        this.articulo = articulo;
+        this.cantidad = cantidad;
+    }
+
+    public Articulo getArticulo() {
+        return articulo;
+    }
+
+    public void setArticulo(Articulo articulo) {
+        this.articulo = articulo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
---
+++ src/modelo/Ubicacion.java
@@ -0,0 +1,30 @@
+package modelo;
+
+public class Ubicacion {
+    private double latitud;
+    private double longitud;
+
+    public Ubicacion(double latitud, double longitud) {
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+}
+
+
+
